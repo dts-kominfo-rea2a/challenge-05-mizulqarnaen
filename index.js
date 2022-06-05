7f5ec9e06eca7ef802c68ad52a5655bfe0a2dfe8
Mohammad Iqbal <mohammad.iqbal.2409@gmail.com>
2022-06-05 23:57:48 +0700

    Membuat fungsi sorter dengan callback

--- index.js
+++ index.js
@@ -6,15 +6,29 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (data, cbSort) => {
+  const sortedData = cbSort(data);
+  const listNama = []
+  
+  for (let index = 0; index < sortedData.length; index++) {
+    const name = sortedData[index];
+    listNama.push((index + 1) + ". " + name);
+  }
+
+  return listNama;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => {
+  return data.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) => {
+  return data.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
